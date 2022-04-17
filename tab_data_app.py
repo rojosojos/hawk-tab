3c328190b43d6e1b292bfa721808a5817526a18d
Terry Menteer <terrymenteer@gmail.com>
2022-04-16 20:53:16 -0400

    Basic Pressure Altitude dropdown menu

--- tab_data_app.py
+++ tab_data_app.py
@@ -14,21 +14,22 @@ class MainInterface(MDBoxLayout):
 
         press_alts = [0,500,1000,1500,2000,2500,3000,3500,4000,4500,5000,5500,6000,6500,7000,7500,8000,8500,9000,9500,10000,10500,11000,11500,12000,12500,13000]
 
-        menu_items = [
+        pa_menu_items = [
             {
                 "text": f"PA: {i}",
                 "viewclass": "OneLineListItem",
-                "on_release": lambda x=i: self.menu_callback(x),
+                "on_release": lambda x=i: self.pa_menu_callback(x),
             } for i in press_alts
         ]
         self.menu = MDDropdownMenu(
             caller=self.ids.button,
-            items=menu_items,
+            items=pa_menu_items,
             width_mult=4,
         )
 
-    def menu_callback(self, text_item):
-        print(text_item)
+    def pa_menu_callback(self, text_item):
+        self.ids.pa_label.text = str(text_item)
+        # print(text_item)
 
 class TabDataApp(MDApp):
 
