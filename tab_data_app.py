1094c8ea919d36f5177e2fee3a09b12274157701
Terry Menteer <terrymenteer@gmail.com>
2022-04-16 21:56:00 -0400

    Add OAT dropdown Menu

--- tab_data_app.py
+++ tab_data_app.py
@@ -16,6 +16,7 @@ class MainInterface(MDBoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        ## PRESSURE ALTITUDE MENU ##
         press_alts = [0,500,1000,1500,2000,2500,3000,3500,4000,4500,5000,5500,6000,6500,7000,7500,8000,8500,9000,9500,10000,10500,11000,11500,12000,12500,13000]
 
         pa_menu_items = [
@@ -26,15 +27,35 @@ class MainInterface(MDBoxLayout):
             } for i in press_alts
         ]
         self.pa_menu = MDDropdownMenu(
-            caller=self.ids.button,
+            caller=self.ids.pa_button,
             items=pa_menu_items,
             width_mult=4,
         )
 
-    def pa_menu_callback(self, text_item):
-        self.ids.pa_label.text = str(text_item)
+        ## OUTSIDE AIR TEMP MENU ##
+        outside_air_temps = [-15,-10,-5,0,5,10,15,20,25,30,35,40]
+
+        oat_menu_items = [
+            {
+                "text":f"{oat}",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x=oat: self.oat_menu_callback(x),
+            } for oat in outside_air_temps
+        ]
+        self.oat_menu = MDDropdownMenu(
+            caller=self.ids.oat_button,
+            items=oat_menu_items,
+            width_mult=4,
+        )
+
+    def pa_menu_callback(self, menu_item):
+        self.ids.pa_label.text = str(menu_item)
         self.pa_menu.dismiss()
-        # print(text_item)
+
+    def oat_menu_callback(self, menu_item):
+        self.ids.oat_label.text = str(menu_item)
+        self.oat_menu.dismiss()
+
 
 class TabDataApp(MDApp):
 
